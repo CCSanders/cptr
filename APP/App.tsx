7094b3f1f7248bef89530d2cfdcc7ba7631210c2
Colin Sanders <ck.sanders4@gmail.com>
2021-12-15 17:37:29 -0600

    safe area app container test

--- APP/App.tsx
+++ APP/App.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppNavigator from './app/navigation';
 
-const App = () => (
-  <AppNavigator />
-);
+const SafeAreaApp = () => {
+  return (
+    <SafeAreaProvider>
+      <AppNavigator />
+    </SafeAreaProvider>
+  )
+}
+
+const App = () => {
+  return (
+    <SafeAreaApp/>
+  )
+}
 
 export default App;
