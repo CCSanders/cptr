18355f4b3be8807e3470c7b32c92758ff468f924
Colin Sanders <ck.sanders4@gmail.com>
2022-02-27 16:31:23 -0600

    initial capture logic

---
+++ APP/app/screens/CaptureScreen/components/CaptureButton.tsx
@@ -0,0 +1,235 @@
+import React, { useCallback, useMemo, useRef } from 'react';
+import { StyleSheet, View, ViewProps } from 'react-native';
+
+import {
+    PanGestureHandler,
+    PanGestureHandlerGestureEvent,
+    State,
+    TapGestureHandler,
+    TapGestureHandlerStateChangeEvent,
+} from 'react-native-gesture-handler';
+
+import Reanimated, {
+    cancelAnimation,
+    Easing,
+    Extrapolate,
+    interpolate,
+    useAnimatedStyle,
+    withSpring,
+    withTiming,
+    useAnimatedGestureHandler,
+    useSharedValue,
+    withRepeat,
+} from 'react-native-reanimated';
+
+import type { Camera, PhotoFile, TakePhotoOptions, TakeSnapshotOptions, VideoFile } from 'react-native-vision-camera';
+import { createAsyncDelay } from 'utils';
+
+const CAPTURE_BUTTON_SIZE = 78;
+const BORDER_WIDTH = CAPTURE_BUTTON_SIZE * 0.1;
+
+interface Props extends ViewProps {
+    camera: React.RefObject<Camera>;
+    onCapture: (photo_array: PhotoFile[]) => void;
+
+    enabled: boolean;
+    setIsPressingButton: (isPressingButton: boolean) => void;
+};
+
+const CaptureButton: React.FC<Props> = ({
+    camera,
+    onCapture,
+    enabled,
+    setIsPressingButton,
+    style,
+    ...props
+}): React.ReactElement => {
+
+    const isPressingButton = useSharedValue(false);
+
+    const takeCPTR = useCallback(async () => {
+        try {
+
+            if (camera.current == null) {
+                throw new Error('Camera ref is null within CaptureButton.tsx');
+            }
+
+            //Todo: additional log context?
+            console.log('Taking photo...');
+
+            await createAsyncDelay(100);
+
+            // play flash effect
+
+            const photo1 = await camera.current.takePhoto({
+                photoCodec: 'png',
+                qualityPrioritization: 'speed',
+                flash: false,
+                quality: 90,
+                skipMetadata: true
+            });
+
+            await createAsyncDelay(100);
+
+            // play flash effect
+
+            const photo2 = await camera.current.takePhoto({
+                photoCodec: 'png',
+                qualityPrioritization: 'speed',
+                flash: false,
+                quality: 90,
+                skipMetadata: true
+            });
+
+            await createAsyncDelay(100);
+
+            // play flash effect
+
+            const photo3 = await camera.current.takePhoto({
+                photoCodec: 'png',
+                qualityPrioritization: 'speed',
+                flash: false,
+                quality: 90,
+                skipMetadata: true
+            });
+
+            await createAsyncDelay(100);
+
+            // play flash effect
+
+            const photo4 = await camera.current.takePhoto({
+                photoCodec: 'png',
+                qualityPrioritization: 'speed',
+                flash: false,
+                quality: 90,
+                skipMetadata: true
+            });
+
+            const photo_array = [photo1, photo2, photo3, photo4, photo4, photo3, photo2, photo1];
+
+            onCapture(photo_array);
+
+        } catch (error) {
+            console.error("Failed to take a photo: ", error)
+        }
+    }, [camera, onCapture])
+
+    const tapHandler = useRef<TapGestureHandler>();
+    const onHandlerStateChanged = useCallback(
+        async ({ nativeEvent: event }: TapGestureHandlerStateChangeEvent) => {
+            switch (event.state) {
+                case State.BEGAN: {
+                    console.log('Capture button pressed.')
+                    setIsPressingButton(true);
+                }
+                case State.END:
+                case State.FAILED:
+                case State.CANCELLED: {
+                    console.log('Capture button released. Begin GIF capture')
+                    try {
+                        await takeCPTR();
+                    } finally {
+                        setTimeout(() => {
+                            isPressingButton.value = false;
+                            setIsPressingButton(false);
+                        }, 500);
+                    }
+                }
+                default:
+                    break;
+            }
+        },
+        [isPressingButton, setIsPressingButton, takeCPTR]
+    );
+
+    const buttonStyle = useAnimatedStyle(() => {
+        let scale: number;
+        if (enabled) {
+            if (isPressingButton.value) {
+                scale = withRepeat(
+                    withSpring(1, {
+                        stiffness: 100,
+                        damping: 1000,
+                    }),
+                    -1,
+                    true,
+                );
+            } else {
+                scale = withSpring(0.9, {
+                    stiffness: 500,
+                    damping: 300,
+                });
+            }
+        } else {
+            scale = withSpring(0.6, {
+                stiffness: 500,
+                damping: 300,
+            });
+        }
+
+        return {
+            opacity: withTiming(enabled ? 1 : 0.3, {
+                duration: 100,
+                easing: Easing.linear,
+            }),
+            transform: [
+                {
+                    scale: scale,
+                },
+            ],
+        };
+    }, [enabled, isPressingButton]);
+
+    const shadowStyle = useAnimatedStyle(
+        () => ({
+            transform: [
+                {
+                    scale: withSpring(isPressingButton.value ? 1 : 0, {
+                        mass: 1,
+                        damping: 35,
+                        stiffness: 300,
+                    }),
+                },
+            ],
+        }),
+        [isPressingButton],
+    );
+
+    return (
+        <TapGestureHandler
+            enabled={enabled}
+            ref={tapHandler}
+            onHandlerStateChange={onHandlerStateChanged}
+            shouldCancelWhenOutside={false}
+        >
+            <Reanimated.View {...props} style={[buttonStyle, style]}>
+                <Reanimated.View style={styles.flex}>
+                    <Reanimated.View style={[styles.shadow, shadowStyle]} />
+                    <View style={styles.button} />
+                </Reanimated.View>
+            </Reanimated.View>
+        </TapGestureHandler>
+    );
+};
+
+export default CaptureButton;
+
+const styles = StyleSheet.create({
+    flex: {
+        flex: 1,
+    },
+    shadow: {
+        position: 'absolute',
+        width: CAPTURE_BUTTON_SIZE,
+        height: CAPTURE_BUTTON_SIZE,
+        borderRadius: CAPTURE_BUTTON_SIZE / 2,
+        backgroundColor: '#e34077',
+    },
+    button: {
+        width: CAPTURE_BUTTON_SIZE,
+        height: CAPTURE_BUTTON_SIZE,
+        borderRadius: CAPTURE_BUTTON_SIZE / 2,
+        borderWidth: BORDER_WIDTH,
+        borderColor: 'white',
+    },
+});
--- APP/app/screens/CaptureScreen/index.tsx
+++ APP/app/screens/CaptureScreen/index.tsx
@@ -3,18 +3,31 @@ import { Image, StyleSheet, Text, View } from 'react-native';
 import { useIsFocused } from '@react-navigation/core';
 
 import { Camera, CameraRuntimeError, frameRateIncluded, useCameraDevices, PhotoFile } from 'react-native-vision-camera';
+import Reanimated, { Extrapolate, interpolate, useAnimatedGestureHandler, useAnimatedProps, useSharedValue } from 'react-native-reanimated';
 
 import Logo from '@app/assets/images/react-logo.png';
 import { Theme, MaterialColors, FontWeights, FontSizes } from '@app/theme';
-
-
+import CaptureButton from './components/CaptureButton';
 
 const CaptureScreen = () => {
 
   const camera = useRef<Camera>(null);
 
+  const devices = useCameraDevices();
+  const device = devices.back;
+
   const [isCameraInitialized, setIsCameraInitialized] = useState(false);
   const isFocused = useIsFocused();
+
+  const isPressingButton = useSharedValue(false);
+
+  //#region Callbacks
+  const setIsPressingButton = useCallback(
+    (_isPressingButton: boolean) => {
+      isPressingButton.value = _isPressingButton;
+    },
+    [isPressingButton],
+  );
 
   const onInitialized = useCallback(() => {
     console.log('Camera initialized!');
@@ -25,8 +38,14 @@ const CaptureScreen = () => {
     console.error(error);
   }, []);
 
-  const devices = useCameraDevices();
-  const device = devices.back;
+  const onCapture = useCallback((photo_array: PhotoFile[]) => {
+    console.log(`Media captured! ${JSON.stringify(photo_array)}`);
+    
+    //process into gif
+    //go to page that shows gif before publishing.. 
+
+
+  }, [])
 
 
   return (
@@ -41,6 +60,15 @@ const CaptureScreen = () => {
           onError={onError}
         />
       )}
+
+      <CaptureButton
+        style={styles.captureButton}
+        camera={camera}
+        onCapture={onCapture}
+
+        enabled={isCameraInitialized && isFocused}
+        setIsPressingButton={setIsPressingButton}
+      />
     </View>
   );
 };
@@ -62,6 +90,20 @@ const styles = StyleSheet.create({
   logo: {
     height: 64,
     width: 64,
+  },
+  captureButton: {
+    position: 'absolute',
+    alignSelf: 'center',
+    bottom: 10,
+  },
+  button: {
+    marginBottom: 10,
+    width: 78,
+    height: 78,
+    borderRadius: 78 / 2,
+    backgroundColor: 'rgba(140, 140, 140, 0.3)',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   heading: {
     ...FontWeights.Light,
